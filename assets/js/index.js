2296a55de13e865b1e7b7e63e36eae729d3a7b9b
seong <sngoneee@gmail.com>
2024-09-18 16:32:26 +0900

    반응형 수정

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,117 +1,127 @@
 
 
 
-    // --------------- 전체 부드러운 스크롤 --------------- 
-    const lenis = new Lenis({
-      duration: 2,
-      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-      wrapper: document.querySelector('.lenis-wrap'),
-      content: document.querySelector('.inner-lenis'),
-    })
-    lenis.on('scroll', (e) => {
-      console.log(e)
-    })
-    lenis.on('scroll', ScrollTrigger.update)
-    gsap.ticker.add((time)=>{
-      lenis.raf(time * 600)
-    })
-    gsap.ticker.lagSmoothing(0)
+  // --------------- lenis scroll --------------- 
+  const lenis = new Lenis({
+    duration: 2,
+    easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+    // wrapper: document.querySelector('.lenis-wrap'),
+    // content: document.querySelector('.inner-lenis'),
+  })
+  lenis.on('scroll', (e) => {
+    console.log(e)
+  })
+  lenis.on('scroll', ScrollTrigger.update)
+  gsap.ticker.add((time)=>{
+    lenis.raf(time * 600)
+  })
+  gsap.ticker.lagSmoothing(0)
 
 
 
 
-  // --------------- cursor 커스텀 (보류) --------------- 
-  // let mouseCursor = document.querySelector(".cursor");
-  // window.addEventListener("mousemove", (e) => {
-  //   gsap.to(mouseCursor, {
-  //     left: e.pageX + "px",
-  //     top: e.pageY - scrollY + "px",
-  //     ease: "power1.out" 
-  //   });
-  // });
-  
-  // window.addEventListener("scroll", (e) => {
-  //   gsap.to(mouseCursor, {
-  //     top: e.pageY - scrollY + "px",
-  //     ease: "power1.out"
-  //   });
-  // });
-
-
-
-
-
-
-
-
-
-  if (window.matchMedia("(min-width: 1024px)").matches) {
-
-
-    var spans = document.querySelectorAll('#sct span');
-
-spans.forEach(function(span, index) {
-    var currentScramble = new ScrambleText(
-        span,
-        {
-          timeOffset: 1000 + index * 100, 
-          chars: [
-            'A', 'B', 'C', 'D', 'E',
-            'F', 'G', 'H', 'I', 'J',
-            'K', 'L', 'M', 'N', 'O',
-            'P', 'Q', 'R', 'S', 'T', 'U',
-            'V', 'W', 'X', 'Y', 'Z'
-          ],
-          callback: function () {
-              if (index === spans.length - 1)  {
-                document.querySelectorAll('.intro #sct').forEach(function(element) {
-                  element.classList.add('highlight');
-                });
-                setTimeout(()=> {
-                  startIntroMotion();
-                },500)
-              }
-          },
-        }
-    );
-    currentScramble.start();
-});
-
-
-
-function startIntroMotion() {
-  const introMotion = gsap.timeline({
-    scroller: '.lenis-wrap', 
-  });
-  introMotion
-    .to('.guage-area p', {
-      scale:1.3,
-      autoAlpha:0
-    }, 'display+=0.8')
-    .to('.intro', {autoAlpha: 0})
-    .from(".sc-visual .hero-line span", 1.8,
-    {
-        y: 100,
-        ease: "power4.out",
-        skewY: 7,
-        yPercent: 100,
-        stagger: {
-          amount: 1
-        },
+  // --------------- cursor custom --------------- 
+  let mouseCursor = document.querySelector(".cursor");
+  window.addEventListener("mousemove", (e) => {
+    gsap.to(mouseCursor, {
+      left: e.pageX + "px",
+      top: e.pageY - scrollY + "px",
     });
-}
+  });
+  
+  window.addEventListener("scroll", (e) => {
+    gsap.to(mouseCursor, {
+      top: e.pageY - scrollY + "px",
+    });
+  });
+
+
+
+
+  // --------------- header time --------------- 
+  function updateTime() {
+    var today = new Date();   
+    var hours = ('0' + today.getHours()).slice(-2); 
+    var minutes = ('0' + today.getMinutes()).slice(-2);
+    var seconds = ('0' + today.getSeconds()).slice(-2); 
+    var timeString = hours + ':' + minutes  + ':' + seconds;
+    document.getElementById("time").innerHTML = timeString;
+  }
+  setInterval(updateTime, 1000);
+  updateTime();
+
+
+
+
+
+  // --------------- 태블릿 이상 intro ~ sc-visual --------------- 
+  if (window.matchMedia("(min-width: 1024px)").matches) {
+    var spans = document.querySelectorAll('#sct span');
+    spans.forEach(function(span, index) {
+        var currentScramble = new ScrambleText(
+            span,
+            {
+              timeOffset: 500 + index * 50, 
+              callback: function () {
+                if (index === spans.length - 1)  {
+                  document.querySelectorAll('.intro #sct').forEach(function(element) {
+                    element.classList.add('highlight');
+                  });
+                  setTimeout(()=> {
+                    wordChange();
+                  },500)
+                }
+              },
+            }
+        );
+        currentScramble.start();
+    });
+    function wordChange() {
+      const text = 'portfolio'; 
+      const spans = document.querySelectorAll('#sct span');
+      
+      spans.forEach((span, index) => {
+        if (index < text.length) {
+          setTimeout(() => {
+              span.textContent = text[index];  // 한 글자씩 넣기
+          }, index * 100);  // 각 글자가 0.1초간격으로 들어감
+        }
+      });
+      setTimeout(()=> {
+        startIntroMotion();
+      }
+    ,500)
+
+    function startIntroMotion() {
+      const introMotion = gsap.timeline({
+        // scroller: '.lenis-wrap', 
+      });
+      introMotion
+        .to('.guage-area p', {
+          scale:1.3,
+          autoAlpha:0
+        }, 'display+=0.8')
+        .to('.intro', {autoAlpha: 0})
+        .from('#header', {yPercent: -100, duration:1}, "introEnd")
+        .from(".sc-visual .hero-line span", 1.8,
+        {
+          y: 100,
+          ease: "power4.out",
+          skewY: 7,
+          yPercent: 100,
+          stagger: {
+            amount: 1
+          },
+        }, "introEnd")
+      }
+  }
 
 
 
 
 
 
-
-
-
-
-
-  // --------------- sc-visual --------------- 
+  // ---------------  태블릿 이상 sc-visual --------------- 
   const mainTxt = gsap.timeline({
     scrollTrigger: {
       trigger: '.sc-visual .sticky-wrapper',
@@ -123,7 +133,7 @@ function startIntroMotion() {
       stagger: {
         amount: 1,
       },
-      scroller: '.lenis-wrap', 
+      // scroller: '.lenis-wrap', 
     },
   });
   
@@ -147,10 +157,6 @@ function startIntroMotion() {
     .to('.sc-visual', {
       autoAlpha: 0,
     });
-
-
-
-
   }
 
 
@@ -159,23 +165,15 @@ function startIntroMotion() {
 
 
 
-
-
-
-
-
-
-// --------------------  sc-main -------------------- 
+  // -------------------- 태블릿 이상 sc-main -------------------- 
   function scmainAnimation() {
     if (window.matchMedia("(min-width: 1024px)").matches) {
 
-      // prj-item hover cursor
+      // prj-item hover 1) cursor
       document.addEventListener("mousemove", (e) => {
         const x = e.clientX;
         const y = e.clientY;
-
         $("#cursor_div").css('transform', 'translate(' + x + 'px, ' + y + 'px)');
-
         $('.prj-item .thumb-wrap').on('mouseover', function () {
           $('.inner_wrap').addClass('on');
           $('.cursor').css('display','none');
@@ -188,20 +186,19 @@ function startIntroMotion() {
         });
       });
 
-
-      // prj-item hover scramble
+      // prj-item hover 2) scramble
       $(".prj-item").each(function (index, element){
         if (!element.animation) {
           var currentScramble = new ScrambleText( 
             document.getElementById('tit-scr0' + (index+1)),
             {
-              timeOffset: 300,
+              timeOffset: 100 + index * 10, 
               chars: [
                 'A', 'B', 'C', 'D', 'E',
                 'F', 'G', 'H', 'I', 'J',
                 'K', 'L', 'M', 'N', 'O',
                 'P', 'Q', 'R', 'S', 'T', 'U',
-                'V', 'W', 'X', 'Y', 'x'
+                'V', 'W', 'X', 'Y', 'Z'
               ],
             }
           );
@@ -215,14 +212,12 @@ function startIntroMotion() {
           this.animation.start().play();
         }
       });
-
       $(".prj-item").on('mouseleave', function () {
-        this.animation.stop(0.2);
+        this.animation.stop(1);
         this.animationStopped = true;  
       });
     } else {
       document.removeEventListener("mousemove", null);
-
       $(".prj-item .thumb-wrap").off('mouseover mouseleave');
       $(".prj-item").off('mouseenter mouseleave');
     }
@@ -234,42 +229,37 @@ function startIntroMotion() {
 
 
 
-
-  
-
-
-// -------------------- sc-fe -------------------- 
+  // -------------------- 태블릿 이상 sc-fe -------------------- 
   let scfeTimeline = null;
   function scfeAnimation() {
-    if (window.matchMedia("(min-width: 1024px)").matches) {
+    if (window.matchMedia("(min-width: 1025px)").matches) {
       // sc-fe yper
-      // if (!scfeTimeline) {
-      //   scfeTimeline = gsap.timeline({
-      //     scrollTrigger: {
-      //       trigger: '.sc-fe',
-      //       start: '10% 80%',
-      //       end: '0% 0%',
-      //       scrub: 1,
-      //       ease: "power4.out",
-      //       skewY: 7,
-      //       scroller: '.lenis-wrap', 
-      //     },
-      //   });
-      //   scfeTimeline
-      //   .fromTo('.sc-fe .fe-list', {
-      //     yPercent: 10,
-      //   }
-      // , {
-      //   yPercent: -5,
-      // });
-      // }
+      if (!scfeTimeline) {
+        scfeTimeline = gsap.timeline({
+          scrollTrigger: {
+            trigger: '.sc-fe',
+            start: '10% 80%',
+            end: '0% 0%',
+            scrub: 1,
+            ease: "power4.out",
+            skewY: 7,
+            // scroller: '.lenis-wrap', 
+          },
+        });
+        scfeTimeline
+        .fromTo('.sc-fe .fe-list', {
+          yPercent: 10,
+        }
+      , {
+        yPercent: -5,
+      });
+      }
 
       // fe-item hover
       $(".fe-item").each(function (index, element){
         if (!element.animation) {
           var tl = gsap.timeline({
             paused:true,
-            scroller: '.lenis-wrap',
             ease: "power4.out",
             skewY: 7,
           });
@@ -280,68 +270,28 @@ function startIntroMotion() {
             {
               y: '-200',
             },'img')
+            .fromTo($(element).find(".thumb-area .link-box"), {
+              yPercent: -100,
+            },
+            {
+              yPercent: 0,
+            },'img')
           .fromTo($(element).find(".hover-item img"), {
             yPercent: -100,
           },
           {
             yPercent: 0,
             stagger :0.1,
-          },'img');
+          },'img')
           element.animation = tl;
         }
       });
-    
-      // $(".fe-item").off('mouseenter mouseleave');
       $(".fe-item").on('mouseenter', function() {
         this.animation.play();
       });
       $(".fe-item").on('mouseleave', function() {
         this.animation.reverse(0.5);
       });
-
-
-      // work-item hover
-    //   $('.fe-item').hover(function() {
-    //     $(".fe-item").each(function (index, element){
-    //       if (!element.animation) {
-    //         var tl = gsap.timeline({
-    //           paused:true,
-    //           scroller: '.lenis-wrap',
-    //         });
-    //         tl
-    //         .to($(element).find(".thumb-area p"), {
-    //             yPercent: '-300',
-    //         },'img')
-    //         .to($(element).find(".hover-item li"), {
-    //           yPercent: '100',
-    //           stagger :0.1,
-    //         },'img');
-    //         element.animation = tl;
-    //       }
-    //     });
-    //   }
-    //   , function() {
-    //     $(".fe-item").each(function (index, element){
-    //       if (!element.animation) {
-    //         var tlst = gsap.timeline({
-    //           paused:true,
-    //           scroller: '.lenis-wrap',
-    //         });
-    //         tl
-    //         .to($(element).find(".thumb-area p"), {
-    //             yPercent: '0',
-    //           },'imgstp')
-    //           .to($(element).find(".hover-item li"), {
-    //             yPercent: '0',
-    //             stagger :0.1,
-    //           },'imgstp');
-    //         element.animation = tlst;
-    //       }
-    //     });
-    //   }
-    // );
-
-
     } else {
       // sc-fe yper
       if (scfeTimeline) {
@@ -370,7 +320,7 @@ function startIntroMotion() {
 // -------------------- sc-work -------------------- 
   let scworkTimeline = null;
   function scworkAnimation() {
-    if (window.matchMedia("(min-width: 1024px)").matches) {
+    if (window.matchMedia("(min-width: 1441px)").matches) {
 
       // sc-work yper
       if (!scworkTimeline) {
@@ -381,7 +331,7 @@ function startIntroMotion() {
             end: '10% 10%',
             scrub: 1,
             ease: "none",
-            scroller: '.lenis-wrap', 
+            // scroller: '.lenis-wrap', 
           },
         });
         scworkTimeline.to('.sc-work .work-list', {
@@ -396,7 +346,7 @@ function startIntroMotion() {
           xPercent: -3,
           yPercent: 2,
           ease: "linear",
-          scroller: '.lenis-wrap', 
+          // scroller: '.lenis-wrap', 
         });
       }
       , function() {
@@ -405,12 +355,11 @@ function startIntroMotion() {
           xPercent: 0,
           yPercent: 0,
           ease: "linear",
-          scroller: '.lenis-wrap', 
+          // scroller: '.lenis-wrap', 
         });
       }
     );
     } else {
-
       // sc-work yper
       if (scworkTimeline) {
         scworkTimeline.kill(); // GSAP 애니메이션 제거
@@ -419,8 +368,6 @@ function startIntroMotion() {
       $('.sc-work .work-item').off('mouseenter mouseleave');
     }
   }
-
-
   scmainAnimation();
   scfeAnimation();
   scworkAnimation();
